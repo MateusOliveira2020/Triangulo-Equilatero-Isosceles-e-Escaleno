b11a89a9be486e5a4b442273bb5d4c5b1552f1f1
Mateus de <mateusoliveira.4959@aluno.saojudas.br>
2020-10-28 00:09:18 +0000

    Initial commit

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/*Entre com três medidas para um triângulo e verifique que tipo de triângulo ele forma: equilátero(3 lados iguais), isósceles (2 lados iguais) ou escaleno (3 lados diferentes). Somente verifique se for um triângulo válido.
+- Entrar com dois valores. Imprimir uma das mensagens abaixo, conforme a condição:
+    - Número iguais
+    - Primeiro número é maior
+    - Segundo número é maior
+    */
+int main(void) {
+  int lA=0;
+  int lB=0;
+  int lC=0;
+  int soma =0
+  printf("Digite o valor de lA\n");
+  scanf("%i",&lA);
+  printf("Digite o valor de lB\n");
+  scanf("%i",&lB);
+  printf("Digite o valor de lC\n");
+  scanf("%i", &lC);
+  if 
+
+  return 0;
+}
